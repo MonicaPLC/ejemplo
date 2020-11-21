362032e79754e546b07ad6d98455a338879c4768
Monica <cmonikam@yahoo.es>
2020-11-21 12:29:42 -0300

    cambios iniciales

---
+++ ejemplo.js
@@ -0,0 +1,28 @@
+for(var i=0; i<5;i++){   
+  console.log(i);
+}
+
+for(var i=0; i<5;i++){   
+  console.log(i);
+}
+
+for(var i=0; i<5;i++){   
+  console.log(i);
+}
+
+for(var i=0; i<5;i++){   
+  console.log(i);
+}
+
+
+hola mundo
+
+como diferencias comit local y remoto
+
+OTRA MODIFICACION
+
+nuevos cambios
+
+mas informacion
+
+otro cambio
